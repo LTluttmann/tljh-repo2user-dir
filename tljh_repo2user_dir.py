0732995edfb1d96e88aef2da1d15746355c3be72
Laurin Luttmann <laurin.luttmann@gmail.com>
2023-04-26 18:30:42 +0200

    cleanup

--- tljh_repo2user_dir.py
+++ tljh_repo2user_dir.py
@@ -3,19 +3,12 @@ import shutil
 from pwd import getpwnam
 from git import Repo
 from tljh.hooks import hookimpl
-import logging
-
-
-logger = logging.getLogger(__name__)
-flog = logging.FileHandler("/home/tdi2023/plugin.log")
-flog.setFormatter(logging.Formatter("%(asctime)s %(message)s"))
-logger.addHandler(flog)
-logger.setLevel(logging.INFO)
 
 
 def clone_repo(user, git_url, repo_dir):
     """
-    A function to clone a github repo into a specific directory of a user.
+    A function to clone a github repo into a specific directory of a given user.
+    User receiver owner rights of all files in the repository
     """
     Repo.clone_from(git_url, repo_dir)
     uid = getpwnam(user).pw_uid
@@ -30,27 +23,21 @@ def clone_repo(user, git_url, repo_dir):
 @hookimpl
 def tljh_new_user_create(username):
     """
-    A function to clone a github repo into a specific directory of a
+    A function to clone a github repo into a 'repos' directory for every
     JupyterHub user when the server spawns a new notebook instance.
-    args: spawner is of type tljh UserCreateSpawner
     """
-    logger.info("Gonna clone for user %s" % username)
-    logger.info("Following env vars are available %s" % os.environ)
     user_root_dir = os.path.join("/home", username)
     # get repo url from environment variable
-    git_url = os.getenv("REPO_URL", "https://github.com/LTluttmann/tljh-repo2user-dir.git")
+    git_url = os.getenv("REPO_URL")
     # nothing to do if no repo is specified
     if git_url is None:
-        logger.warning("No repo url found in environment variables. Skip cloning")
         return
     
-    logger.info("Cloning repo %s" % git_url)
     repo_dir = os.path.join(user_root_dir, 'repos')
 
     if not os.path.isdir(repo_dir):
         os.makedirs(repo_dir)
         clone_repo(username, git_url, repo_dir)
-        logger.info("Done cloning")
     else:
         # user already has the repo downloaded
-        logger.info("User already got repo")
+        pass
