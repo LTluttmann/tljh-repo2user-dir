5bd45950fcfc2e023b5a886038f948a2278dea1e
Laurin Luttmann <laurin.luttmann@gmail.com>
2023-04-26 15:47:15 +0200

    basic logging

--- tljh_repo2user_dir.py
+++ tljh_repo2user_dir.py
@@ -3,6 +3,14 @@ import shutil
 from pwd import getpwnam
 from git import Repo
 from tljh.hooks import hookimpl
+import logging
+
+
+logger = logging.getLogger(__name__)
+flog = logging.FileHandler("/home/tdi2023/usrhooks.log")
+flog.setFormatter(logging.Formatter("%(asctime)s %(message)s"))
+logger.addHandler(flog)
+logger.setLevel(logging.INFO)
 
 
 def clone_repo(user, git_url, repo_dir):
@@ -26,19 +34,22 @@ def tljh_new_user_create(username):
     JupyterHub user when the server spawns a new notebook instance.
     args: spawner is of type tljh UserCreateSpawner
     """
-
+    logger.info("Gonna clone for user %s" % username)
     user_root_dir = os.path.join("/home", username)
     # get repo url from environment variable
     git_url = "https://github.com/LTluttmann/tljh-repo2user-dir.git" # os.getenv('REPO_URL')
     # nothing to do if no repo is specified
     if git_url is None:
+        logger.warning("No repo url found in environment variables. Skip cloning")
         return
-
+    
+    logger.info("Cloning repo %s" % git_url)
     repo_dir = os.path.join(user_root_dir, 'repos')
 
     if not os.path.isdir(repo_dir):
         os.makedirs(repo_dir)
         clone_repo(username, git_url, repo_dir)
+        logger.info("Done cloning")
     else:
         # user already has the repo downloaded
-        pass
+        logger.info("User already got repo")
